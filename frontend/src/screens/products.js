3805c6f722da9c514dccbdeb535d752c176a1c31
jodarko <72194602+johnsondarko3@users.noreply.github.com>
2021-08-22 23:14:10 +0200

    Second Commit
    Front end

---
+++ frontend/src/screens/products.js
@@ -0,0 +1,89 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Airpods Wireless Bluetooth Headphones',
+        image: '/images/airpods.jpg',
+        description:
+        'Blueethooth compatible',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock: 0,
+        rating: 4,
+        numReviews: 4,
+
+    },
+    {
+        _id: '2',
+        name: 'Iphone 11 Bluetooth Headphones',
+        image: '/images/phone.jpg',
+        description:
+        'Blueethooth compatible',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock: 1,
+        rating: 4,
+        numReviews: 4,
+
+        }, 
+        {
+            _id: '3',
+            name: 'Cannon Wireless Bluetooth Headphones',
+        image: '/images/camera.jpg',
+        description:
+        'Blueethooth compatible',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock: 3,
+        rating: 3,
+        numReviews: 9,
+
+    },
+    {
+        _id: '4',
+        name: 'Playstation Bluetooth Headphones',
+        image: '/images/Playstation.jpg',
+        description:
+        'Blueethooth compatible',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock: 6,
+        rating: 4,
+        numReviews: 4,
+
+    },
+    {
+        _id: '5',
+        name: 'Logitech Mouse',
+        image: '/images/mouse.jpg',
+        description:
+        'Blueethooth compatible',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock: 7,
+        rating: 3,
+        numReviews: 5,
+
+    },
+    {
+        _id: '6',
+        name: 'Amazon echo',
+        image: '/images/alexa.jpg',
+        description:
+        'Blueethooth compatible',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 6,
+
+    },
+   
+]
+
+export default products
